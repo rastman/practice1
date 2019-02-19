7cfa3cc0682484638a93c21248e4a3629880ef82
rastman <houman.rastgar@gmail.com>
2019-02-18 18:01:27 -0800

    Update hashh.cpp

--- hashh.cpp
+++ hashh.cpp
@@ -1,1 +1,30 @@
-text
+#include <iostream>
+#include <list>
+
+using namespace std;
+class HashNode
+{
+public:
+    int h_key;
+    int h_value;
+    HashNode(int key, int value)
+    {
+    h_key=key;
+    h_value=value;
+    }
+};
+class HashNode
+{
+public:
+
+
+
+};
+int main(void)
+{
+
+
+
+
+    return 0;
+}
